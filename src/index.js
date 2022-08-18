d59f9140ce9c124c70e6c55b702698ad42593668
Yogesh S <yogeshvishal98@gmail.com>
2022-08-18 15:43:26 +0530

    Add tabs and create project modal box eventlisteners

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-import './style.css';
+import "./style.css";
+import * as homeCreation from "./home-today-UI";
+import {upcomingTabCreation} from './upcoming-UI';
+import { createProjectModalBox,projectEventListener,projectTab } from "./project";
+
+
+homeCreation.mainCreation();
+homeCreation.sidePaneCreation();
+homeCreation.todayTabCreation();
+homeCreation.sidePaneEventListener();
+upcomingTabCreation();
+projectTab('inbox');
+document.getElementById('upcomingTab').style.display = 'none'
+
+const createProject = document.getElementById('createProject')
+createProject.addEventListener('click', () => {
+    if(document.getElementById('projectModalBoxContainer') == null){
+        createProjectModalBox();
+        document.getElementById("projectModalBoxContainer").style.display = "block";
+    }
+    else{
+        document.getElementById("projectModalBoxContainer").style.display = "block";
+    }
+})
+
+projectEventListener();
